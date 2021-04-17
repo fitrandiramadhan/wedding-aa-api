9185f0fc1697af964d57cb72b5ca2ebf8c8d719c
andi <fitrandiramadhan@gmail,com>
2021-04-17 19:55:46 +0700

    adding sanctum auth

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
+// Public Routes
 Route::resource('Food', FoodController::class);
 // Route::get('/Food', [FoodController::Class, 'index']);
 // Route::post('/Food', [FoodController::Class, 'store']);
@@ -29,6 +29,17 @@ Route::resource('Event', EventController::class);
 Route::resource('Guest', GuestController::class);
 Route::resource('Room', RoomController::class);
 Route::resource('Seating', SeatingController::class);
+
+// Protected routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
